548d6094601a36e866e3c79a8214bdb8867edb17
Ankit Agarwal <ankit.agarwal@sky.uk>
2025-10-07 19:01:58 +0100

    Added the log4j2 in automation framework

--- src/main/java/AnkitLearningPath/base/Base.java
+++ src/main/java/AnkitLearningPath/base/Base.java
@@ -5,14 +5,19 @@ import static io.restassured.RestAssured.*;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 import static io.restassured.RestAssured.*;
 
 public class Base {
     private static final String BASE_URI = "https://petstore.swagger.io/v2";
     protected static RequestSpecification requestSpecification = given().baseUri(BASE_URI);
+    protected static Logger logger = LogManager.getLogger(Base.class);
 
     protected static Response postRequest(Object payload, String endpoint) {
+        logger.info("payload and end point pass into base class");
         return requestSpecification
                 .contentType(ContentType.JSON)
                 .body(payload)
--- src/main/java/AnkitLearningPath/services/PetServices.java
+++ src/main/java/AnkitLearningPath/services/PetServices.java
@@ -3,16 +3,21 @@ package AnkitLearningPath.services;
 import AnkitLearningPath.base.Base;
 import AnkitLearningPath.models.pet.requests.PetRequest;
 import io.restassured.response.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 
 public class PetServices extends Base {
+    private static Logger logger = LogManager.getLogger(PetServices.class);
     private static final String ADD_OR_UPDATE_PET = "/pet";
     private static final String ADD_PET_IMAGE = "/pet/{petId}/uploadImage";
     private static final String PET_STATUS = "/pet/findByStatus";
 
 
     public static void addpet(Object petRequest) {
+        logger.info("called the post request method of base class in petservices class");
         Response response = Base.postRequest(petRequest, ADD_OR_UPDATE_PET);
+        logger.info("response got successfully");
         response.then().log().all();
         Assert.assertEquals(response.getStatusCode(),200);
     }
